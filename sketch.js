03040f543a87607d4417e36c9022b0df220f395c
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2019-07-03 19:55:09 +0200

    Updates

--- sketch.js
+++ sketch.js
@@ -1,29 +1,42 @@
-const windowWidth = 800;
+const windowWidth = 600;
 const windowHeight = 600;
 
+let s;
 
 function setup() {
 
-	createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
 
-
-
-
-
+    s = new Snake();
 
 }
 
 
 function draw() {
 
-	background(0);
+    background(51);
 
-	ellipse(40,40,80,80);
-
-
-
-
-
-
+    s.update();
+    s.show();
 
 }
+function keyPressed()
+{
+    if (keyCode === UP_ARROW)
+    {
+        s.dir(0,-1);
+    }
+    else if(keyCode === DOWN_ARROW)
+    {
+        s.dir(0,1);
+    }
+    else if (keyCode === LEFT_ARROW) {
+        s.dir(-1, 0);
+    }
+    else if(keyCode === RIGHT_ARROW)
+    {
+        s.dir(1, 0);
+    }
+}
+
+
---
+++ snake.js
@@ -0,0 +1,26 @@
+
+function Snake() {
+    this.x = 0;
+    this.y = 100;
+
+    this.xspeed = 1;
+    this.yspeed = 0;
+
+    this.update = function () {
+        this.x += this.xspeed;
+        this.y += this.yspeed;
+
+    };
+
+    this.show = function () {
+        fill(255);
+        rect(this.x,this.y,10,10);
+    }
+
+    this.dir= function(x,y)
+    {
+        this.xspeed = x;
+        this.yspeed = y;
+    }
+
+}
