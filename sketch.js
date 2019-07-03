64912d8e0d1ba940a225f2fcefa9e3a7a70ed1e1
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2019-07-03 19:38:47 +0200

    Updates

---
+++ sketch.js
@@ -0,0 +1,29 @@
+const windowWidth = 800;
+const windowHeight = 600;
+
+
+function setup() {
+
+	_createCanvas(windowWidth, windowHeight);
+
+
+
+
+
+
+}
+
+
+function draw() {
+
+	background(0);
+
+	ellipse(40,40,80,80);
+
+
+
+
+
+
+
+}
