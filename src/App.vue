48ff7cd5eb1d436e2a07e205437b5266d228085c
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2020-10-30 12:46:36 +0100

    new page

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="content">
+ <div id="nav">
+          <router-link to="/">Home</router-link>
+
+          <router-link to="/about">About</router-link>
+        </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+@import "@/assets/scss/style";
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div class="hero-image">
+      <div class="hero-text">
+       
+
+        <div class="main">
+          <h1>Mikael</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {}
+});
+</script>
